9b24ef65e8b171b4fef7b56168029a2a7a44b4e7
Kung <sales@caryelectronic.com>
2025-03-20 13:40:41 +0800

    some

--- official/diy-part2.sh
+++ official/diy-part2.sh
@@ -34,7 +34,7 @@ sed -i 's/\(\([ \t]\+\)set network.$1.netmask=.*\)/\1\n \2set network.$1.gateway
 sed -i 's/\(luci-app-bypass\)=y/\1=n/' .config
 sed -i 's/\(luci-app-passwall\)=y/\1=n/' .config
 sed -i 's/\(luci-app-ssr-plus\)=y/\1=n/' .config
-# sed -i 's/\(chinadns-ng\)=y/\1=n/' .config
+sed -i 's/\(netdata\)=y/\1=n/' .config
 sed -i 's/\(zerotier\)=y/\1=m/' .config
 
 # 添加额外软件包
