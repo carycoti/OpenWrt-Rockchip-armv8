81ace7ce9cdd7516aeff480ee42b203a61656240
Kung <sales@caryelectronic.com>
2025-09-03 19:58:25 +0800

    remove redsocks2

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -95,7 +95,7 @@ sed -i 's/PKG_MIRROR_HASH:=.*/PKG_MIRROR_HASH:=skip/' feeds/kiddin9/cgroupfs-mou
 # kiddin9 相关的冲突包
 rm -rf feeds/packages/net/{alist,mosdns,xray*,v2ray*,v2ray*,smartdns}
 rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/kiddin9/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,upx,miniupnpd-iptables,wireless-regdb,quectel_SRPD_PCIE,adguardhome,shortcut-fe,fibocom_QMI_WWAN,quectel_QMI_WWAN,rtl8189es,accel-ppp,dockerd,cgroupfs-mount,lua-maxminddb,luci-app-linkease,luci-app-quickstart,fullconenat-nft}
+rm -rf feeds/kiddin9/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,upx,miniupnpd-iptables,wireless-regdb,quectel_SRPD_PCIE,adguardhome,shortcut-fe,fibocom_QMI_WWAN,quectel_QMI_WWAN,rtl8189es,accel-ppp,dockerd,cgroupfs-mount,lua-maxminddb,luci-app-linkease,luci-app-quickstart,fullconenat-nft,redsocks2}
 mv -f feeds/kiddin9/r81* tmp/
 # 替换 quickstart.lua
 curl -sL https://raw.githubusercontent.com/kenzok8/openwrt-packages/master/luci-app-quickstart/luasrc/controller/quickstart.lua -o feeds/kiddin9/luci-app-quickstart/luasrc/controller/quickstart.lua
